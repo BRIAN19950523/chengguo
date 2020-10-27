1170238f8cd396bcb5671444c82c4b0b438d0d7c
Chengguo <2409813369@qq.com>
2020-10-27 14:18:21 +0800

    addinit

---
+++ exercise01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# result = 1
+# for i in range(1, 21):
+#     result *= i
+# print(result)
---
+++ exercise02.py
@@ -0,0 +1,14 @@
+# def isprime(n):
+#     if n<=1:
+#         return False
+# for i in range(2,n//2+1):
+#     if n % 1 == 0:
+#         return False
+# return True
+#
+# def prime_sum(num:int):
+#     prime = []
+#     for i in range(num + 1):
+#         if isprime(i):
+#             prime.append(i) #是至数
+#     return sum(prime) #求和
